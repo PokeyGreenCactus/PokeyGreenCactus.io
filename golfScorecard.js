b3d66e5a26963e4a50d9a46388b38d300ec1db4c
PokeyGreenCactus <89933864+PokeyGreenCactus@users.noreply.github.com>
2021-10-18 14:47:28 -0400

    Update golfScorecard.js

--- golfScorecard.js
+++ golfScorecard.js
@@ -26,6 +26,10 @@ function add1 (elem) {
     currentScore = Number.parseInt(currentScore);
     elem.children[2].innerHTML = currentScore + 1;
   }
+  calcOverRow(elem);
+  calcScoreTotal(elem);
+  calcOverTotal(elem);
+  clear(elem);
 }
 
 //create a "sub1" function
@@ -41,13 +45,59 @@ function sub1 (elem) {
 		currentScore = Number.parseInt(currentScore);
 		elem.children[2].innerHTML = currentScore - 1;
 	}
+	calcOverRow(elem);
+	calcScoreTotal(elem);
+	calcOverTotal(elem);
+	clear(elem);
 }
 
 function clear (elem) {
-	elem.children[2].innerHTML = "-";
-	document.getElementById('scoreTotal').innerHTML = "-";
+	let currentScore = elem.children[2].innerHTML;
+	currentScore=Number.parseInt(currentScore);
+	let currentOver = elem.children[3].innerHTML;
+	currentOver=Number.parseInt(currentOver);
+	let totalScore = document.getElementById('scoreTotal').innerHTML;
+	totalScore=Number.parseInt(totalScore);
+	let totalOver = document.getElementById('overTotal').innerHTML;
+	totalOver=Number.parseInt(totalOver);
+	document.getElementById('scoreTotal').innerHTML = totalScore-currentScore;
+	document.getElementById('overTotal').innerHTML = totalOver-currentOver;
+	elem.children[2].innerHTML = 0;
+	elem.children[3].innerHTML = 0;
 }
 
+//calculates the total score
+var totalScore = 0;
 function calcScoreTotal (elem) {
-	document.getElementById('scoreTotal').innerHTML = "0";
+	let oneScore = elem.children[2].innerHTML;
+	oneScore = Number.parseInt(oneScore);
+	if(oneScore == "-")
+		oneScore = 0;
+	else if (oneScore == "0") {
+		oneScore = 0;
+	}
+	totalScore++;
+	document.getElementById('scoreTotal').innerHTML = totalScore;
+}
+
+//calculates the total over
+var totalOver = 0;
+function calcTotalOver (elem) {
+	let oneOver = elem.children[3].innerHTML;
+	oneOver = Number.parseInt(oneOver);
+	if(oneOver == "-")
+		oneOver = "0";
+	totalOver++;
+	document.getElementById('overTotal').innerHTML = totalOver;
+}
+
+//create a function that shows the difference between par and score, and outputs it in 'over' for each row
+function calcOverRow (elem) {
+	let par = elem.children[1].innerHTML;
+	let indScore = elem.children[2].innerHTML;
+	let over = elem.children[3].innerHTML;
+	if(indScore == "-")
+		indScore = "0";
+	over = indScore - par;
+	elem.children[3].innerHTML = over;
 }
