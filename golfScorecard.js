68807c8d30059ea0946dfc51118f3898db9caa87
PokeyGreenCactus <89933864+PokeyGreenCactus@users.noreply.github.com>
2021-10-18 17:13:26 -0400

    Update golfScorecard.js

--- golfScorecard.js
+++ golfScorecard.js
@@ -1,16 +1,10 @@
+//Assignment03 - create a dynamic golfScorecard app
+//Author: Adam Cossin
+//Contact: racossin@svsu.edu
+//Date: 10/18/2021
 let elem = [];
-// assign the entire table row for hole 1 to a variable, elem
-// elem[1] = document.getElementById("1");
-
-// display the number of children (all td elements)
-// console.log(elem.children.length);
-// display the content of the + button, which is the first child of the fifth element
-// console.log(elem.children[4].children[0]); 
-
-// assign a function to the + button
 
 for(let i=1; i<=18; i++) {
-  // console.log(i);
 	elem[i] = document.getElementById(i.toString());
 	elem[i].children[4].children[0].onclick = function(){add1(elem[i]);};
 	elem[i].children[4].children[1].onclick = function(){sub1(elem[i]);};
@@ -29,7 +23,6 @@ function add1 (elem) {
   calcOverRow(elem);
   calcScoreTotal(elem);
   calcTotalOver(elem);
-  //clear(elem);
 }
 
 //create a "sub1" function
@@ -48,22 +41,15 @@ function sub1 (elem) {
 	calcOverRow(elem);
 	calcScoreTotal(elem);
 	calcTotalOver(elem);
-	//clear(elem);
 }
 
 function clear (elem) {
-	let currentScore = elem.children[2].innerHTML;
-		currentScore=Number.parseInt(currentScore);
-	let currentOver = elem.children[3].innerHTML;
-		currentOver=Number.parseInt(currentOver);
-	let totalScore = document.getElementById('scoreTotal').innerHTML;
-		totalScore=Number.parseInt(totalScore);
-	//let totalOver = document.getElementById('overTotal').innerHTML;
-		//totalOver=Number.parseInt(totalOver);
-	//if(currentOver<1)
-		//currentOver = 0;
-	//document.getElementById('scoreTotal').innerHTML = totalScore-currentScore;
-	//document.getElementById('overTotal').innerHTML = totalOver-currentOver;
+	let tempScore = elem.children[2].innerHTML;
+	tempScore = Number.parseInt(tempScore);
+	let tempOver = elem.children[3].innerHTML;
+	tempOver = Number.parseInt(tempOver);
+	calcScoreTotal(tempScore);
+	calcTotalOver(tempOver);
 	elem.children[2].innerHTML = 0;
 	elem.children[3].innerHTML = 0;
 }
@@ -75,7 +61,12 @@ function calcScoreTotal (elem) {
 	oneScore = Number.parseInt(oneScore);
 	if(oneScore == "-")
 		oneScore = 0;
-	totalScore++;
+	//I'm not sure why this isn't allowing me to check if the button has been clicked or not, but it's just ignoring this statement
+	if(elem.children[4].children[1].clicked == true)
+		totalScore--;
+	else {
+		totalScore++;
+	}
 	document.getElementById('scoreTotal').innerHTML = totalScore;
 }
 
@@ -85,8 +76,10 @@ function calcTotalOver (elem) {
 	let oneOver = elem.children[3].innerHTML;
 	oneOver = Number.parseInt(oneOver);
 	if(oneOver == "-")
-		oneOver = "0";
-	totalOver+=oneOver;
+		oneOver = 0;
+	if(oneOver <= 0)
+		totalOver--;
+		totalOver++;
 	document.getElementById('overTotal').innerHTML = totalOver;
 }
 
